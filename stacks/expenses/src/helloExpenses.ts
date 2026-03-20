aded6b85644ddb14775303a9881fa026ac5b1428
Kevin Suasnabar <kevin.suasnabar@alegra.com>
2026-03-19 23:10:39 -0500

    Update helloExpenses function message to reflect latest version in GitHub Actions integration

--- stacks/expenses/src/helloExpenses.ts
+++ stacks/expenses/src/helloExpenses.ts
@@ -2,5 +2,5 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import { ok } from "@lambda-project/shared";
 
 export const helloExpenses = async (event: APIGatewayProxyEvent) => {
-  return ok({ message: "Hello from expenses with github actions 2" });
+  return ok({ message: "Hello from expenses with github actions 3" });
 };
